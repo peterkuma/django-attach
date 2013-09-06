978d2fd3800027df824a17cf9f4d2e8e2086de85
Peter Kuma <peterkuma@waveland.org>
2013-09-06 20:20:09 +0200

    example_project: Fix wrong database name

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'db/tjrapid.sqlite'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_DIR, 'db/django-attach.sqlite'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
