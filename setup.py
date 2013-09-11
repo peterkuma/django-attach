464ad166c9d0ded598c9751d9bdb64a275894c45
Peter Kuma <peterkuma@waveland.org>
2013-09-11 19:42:07 +0200

    django-attach 0.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-attach',
-    version='0.1.2',
+    version='0.1.3',
     packages=['django_attach'],
     include_package_data=True,
     license='BSD License',
